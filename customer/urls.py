2dde608ce1512cb2748e3ed5652661261b965738
rufi1214 <rufinogsnchz@gmail.com>
2022-10-26 09:48:49 +0200

    microservices_example with Python using Django as the framework and using Docker

--- customer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from customer.views import CustomerViewSet
-
-urlpatterns = [
-    path('customer/<id>/', CustomerViewSet.as_view(), name='customer_service'),
-    path('customer/', CustomerViewSet.as_view(), name='customer_service'),
-]
